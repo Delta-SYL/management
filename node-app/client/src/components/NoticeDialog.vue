9c26076eae2636724bc84067b6f93c3818ce7cec
Delta-SYL <vxzvxz@163.com>
2022-04-17 16:58:08 +0800

    主页ui+

---
+++ node-app/client/src/components/NoticeDialog.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="dialog">
+        <el-dialog :title="dialog.title" 
+            v-model="dialog.show"
+            :close-on-click-modal='false'
+            :close-on-press-escape='false'>
+            <div class="form">
+                <el-form ref="form" :model="formData" label-width="120px" style="margin:10px;width:auto">
+                    <el-form-item prop="num" label="公告编号：">
+                        <el-input  v-model="formData.num"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="adId" label="发布人员ID：">
+                        <el-input  v-model="formData.adId"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="title" label="标题：">
+                        <el-input  v-model="formData.title"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="date" label="日期：">
+                        <el-input  v-model="formData.date"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="content" label="内容：">
+                        <el-input type="textarea" :rows="15" v-model="formData.content"></el-input>
+                    </el-form-item>
+                    <el-form-item class="text_right">
+                        <el-button type="primary" @click="dialog.show=false">确定</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </el-dialog>
+    </div>      
+</template>
+
+<script>
+export default {
+    name:'dialog',
+    data(){
+    },
+    props:{
+        dialog: Object,
+        formData:Object
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- node-app/client/src/views/Home.vue
+++ node-app/client/src/views/Home.vue
@@ -15,43 +15,40 @@
             </el-col>
             <el-col :span='10'>
               <p class="notice">往期公告</p>
-                  <div class="table">
-        <el-table stripe :data="tableData" style="width: 100%"  max-height="450" border>
+                <div class="table">
+            <el-table stripe :data="tableData" style="width: 95%"  max-height="450" border>
             <el-table-column type="index" label="序号" align="center" width="60"></el-table-column>
             <el-table-column prop="num" label="公告号" width="80" align="center"></el-table-column>
-            <el-table-column prop="title" label="标题" width="350" align="center"></el-table-column>
+            <el-table-column prop="title" label="标题" width="310" align="center"></el-table-column>
             <el-table-column prop="date"  label="发布日期" width="140" align="center"></el-table-column>
             <el-table-column align="center"  width=100 label="详情">
             <template #default="scope">
                 <el-button type="warning" size="small" @click="handleEdit(scope.$index, scope.row)">查看</el-button>
             </template>
             </el-table-column>
-        </el-table>
-        <el-row>
-        <el-col :span='24'>
-        <div class="pagination">
-        <el-pagination
-            :currentPage.sync="paginations.page_index"
-            :page-size="paginations.page_size"
-            :small="small"
-            :disabled="disabled"
-            :background="background"
-            :layout="paginations.layout"
-            :total="paginations.total"
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-        />
-        </div>
-        </el-col>
-        </el-row>
-    </div>
+            </el-table>
+              <el-row>
+                <el-col :span='24'>
+                  <div class="pagination">
+                    <el-pagination
+                      :currentPage.sync="paginations.page_index"
+                      :page-size="paginations.page_size"
+                      :layout="paginations.layout"
+                      :total="paginations.total"
+                      @current-change="handleCurrentChange"
+                    />
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
             </el-col>
         </el-row>
+      <Dialog :dialog="dialog" :formData="formData" @update="getRepair"></Dialog>
     </div>
 </template>
 
 <script>
-//import Dialog from '../components/Dialog.vue';
+import Dialog from '../components/NoticeDialog.vue';
 export default {
     //name:'fundList',
     data(){
@@ -69,12 +66,11 @@ export default {
             noticecontent:"",
             allTableData:[],
             formData:{
-              cid:"",
+              num:"",
               title:"",
               content:"",
-              flag:"",
+              adId:"",
               date:"",
-              handleid:""
             },
             dialog:{
                 show:false,
@@ -105,7 +101,7 @@ export default {
             this.paginations.page_index=1
             this.paginations.page_size=5
             this.tableData=this.allTableData.filter((item,index)=>{
-                console.log(index)
+                //console.log(index)
                 return index<this.paginations.page_size
             })
         },
@@ -122,23 +118,24 @@ export default {
             }
         },
         handleEdit(index,row){
-            //console.log(index)
-            //console.log(row)
             this.dialog={
                 show:true,
-                title:"修改报修单：",
+                title:"查看公告：",
                 option:"edit",
                 type:"complain"
             }
             this.formData={
+              num:row.num,
+              adId:row.adId,
               title:row.title,
               content:row.content,
-              cid:row.cid
+              date:row.date,
             }
+            console.log(this.formData)
         },
     },
     components:{
-        //Dialog
+        Dialog
     }
 }
 </script>
@@ -207,6 +204,12 @@ export default {
 }
 .table{
   margin-top: 5%;
+}
+.pagination{
+    width: 320px;
+    text-align: right;
+    margin-top: 10px;
+     float:right;
 }
 </style>
 
